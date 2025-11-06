8eb40c31e655cbf87f907e221129cf2a53135ba7
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 03:57:56 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:57:32.881Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T00:57:55.490Z
+Commit: Resolve timezone issues in date handling
