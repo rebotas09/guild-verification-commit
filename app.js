07b24d11ac8b8f32244bf2aab4b5dd296b61341b
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 03:58:28 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:58:01.573Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T00:58:27.079Z
+Commit: Add multi-language support
