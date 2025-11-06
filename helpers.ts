20ded27336db449dd8722b62ffc13db8efd38597
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 03:59:21 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:59:11.194Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T00:59:19.915Z
+Commit: Fix memory leak in event handlers
