baf09b46c57d97419a9ac91b072fbba2cd1e595c
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 04:09:55 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:09:37.081Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T01:09:54.373Z
+Commit: Resolve null pointer exception in user service
