e37ee213121fdd0c027d8a1302dda402119b38b0
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 04:09:01 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:08:24.747Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T01:09:00.514Z
+Commit: Resolve null pointer exception in user service
