5a27a6cefbae6a12bde7c5f313b2759bb571a954
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 03:58:02 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:57:34.307Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T00:58:01.573Z
+Commit: Resolve null pointer exception in user service
