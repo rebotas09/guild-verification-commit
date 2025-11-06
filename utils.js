b78346310279a0199026b3e653285e762264d42e
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 04:06:14 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:05:59.728Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T01:06:13.634Z
+Commit: Fix broken links in documentation
