97e5f401f3db2406a3b4830f067e23cb559065d5
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 03:59:36 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:59:25.595Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T00:59:35.161Z
+Commit: Fix broken links in documentation
