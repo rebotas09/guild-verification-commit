b463eb8944b6bc18b27d7fea6236e6c8de888e36
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 03:59:28 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:59:18.403Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T00:59:27.041Z
+Commit: Patch API endpoint error handling
