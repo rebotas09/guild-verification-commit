abcd179ecf6a1d963363dc7c8a285dac40cd3e19
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 04:08:47 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:08:13.236Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T01:08:45.851Z
+Commit: Patch API endpoint error handling
