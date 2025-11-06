486874c43212a7520e67fefd0a52f4c73071ae16
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 04:07:40 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:07:36.041Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T01:07:38.886Z
+Commit: Fix broken links in documentation
