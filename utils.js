ee601c6029fc8cda04d6a31aecc0537714ff0e1c
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 03:59:18 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:58:54.604Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T00:59:16.786Z
+Commit: Fix CSS styling bug on mobile devices
