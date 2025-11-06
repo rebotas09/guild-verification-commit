d5f33479ba729f6d4e3c662ba27eea2a56ac1c6f
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 04:03:38 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:03:29.612Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-06T01:03:37.205Z
+Commit: Fix CSS styling bug on mobile devices
