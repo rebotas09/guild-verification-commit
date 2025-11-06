e29154637a0d6d51a494b418688b7c867d57563b
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 04:05:54 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:05:46.930Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T01:05:52.655Z
+Commit: Fix CSS styling bug on mobile devices
