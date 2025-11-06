95d2a68db29a1da19201d7bfc3c83a939f41aa61
maersp <113216098+rebotas09@users.noreply.github.com>
2025-11-06 04:05:31 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:03:25.840Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T01:05:29.967Z
+Commit: Patch API endpoint error handling
